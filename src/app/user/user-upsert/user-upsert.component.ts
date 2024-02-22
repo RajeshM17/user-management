9f856a4e890ca16c9bddf473cf0b4da377c5678c
Rajesh Malviya <rajeshmalviya1710@gmail.com>
2024-02-22 10:42:02 +0530

    Formbuilder group added

--- src/app/user/user-upsert/user-upsert.component.ts
+++ src/app/user/user-upsert/user-upsert.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user-upsert',
   templateUrl: './user-upsert.component.html',
-  styleUrls: ['./user-upsert.component.scss']
+  styleUrls: ['./user-upsert.component.scss'],
 })
-export class UserUpsertComponent {
+export class UserUpsertComponent implements OnInit {
+  userForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.userForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      address: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      phone: ['', [Validators.required, Validators.pattern('^\\d{10}$')]],
+    });
+  }
+
+  ngOnInit(): void {}
+
+  onSubmit(): void {
+    // Logic to add or update the user in the list
+  }
 }
