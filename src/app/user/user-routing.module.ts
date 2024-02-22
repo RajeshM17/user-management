94e3eacc102d6e03e3ccfe36abaa5d0f8da6666f
Rajesh Malviya <rajeshmalviya1710@gmail.com>
2024-02-22 10:37:16 +0530

    user routing added

--- src/app/user/user-routing.module.ts
+++ src/app/user/user-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserUpsertComponent } from './user-upsert/user-upsert.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'create', component: UserUpsertComponent },
+  { path: 'edit/:id', component: UserUpsertComponent },
+  { path: 'list', component: UserListComponent },
+  { path: '', redirectTo: 'list', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
